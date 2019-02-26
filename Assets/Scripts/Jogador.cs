4b6b2af813bce645c70ac1cc53810fe351641ac0
DavidGomes8 <43244696+DavidGomes8@users.noreply.github.com>
2019-02-26 11:54:33 -0300

    Cena montada
    Configurações basicas:
    -Tiro
    -Movimentação de camera
    -Configuração VR
    -Objetos

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    [Header("Projétil")]
+    [Tooltip("Pega o  projetil a ser lançado")]
+    public GameObject projetil;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        bool botaoPressionado = Input.GetMouseButtonDown(0);
+        if (botaoPressionado)
+        {
+            Atirar();
+        }
+    }
+
+    private void Atirar()
+    {
+        GameObject tiro = Instantiate(projetil);
+        projetil.transform.position = Camera.main.transform.position;
+        projetil.transform.rotation = Camera.main.transform.rotation;
+    }
+}
---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil : MonoBehaviour
+{
+    [Range(1, 5)]
+    public float v = 2f;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //rb.AddForce(100.0f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(v * Time.deltaTime * Vector3.forward);
+    }
+}
