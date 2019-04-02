7cd1c813a3b88a866c2a9146655236f756908c69
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-02 09:49:30 -0300

    Ajustes
    Spawn de zumbi, anda de zumbi, e outros sistemas

--- Assets/Scripts/GeradorZumbis.cs
+++ Assets/Scripts/GeradorZumbis.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeradorZumbis : MonoBehaviour {
+public class GeradorZumbis : MonoBehaviour
+{
+    public float delay = 2f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject zumbip;
+    public float area = 3f;
+
+    // Use this for initialization
+    private void Start()
+    {
+        InvokeRepeating("GerarZumbi", delay, delay);
+    }
+
+    // Update is called once per frame
+    private void GerarZumbi()
+    {
+        GameObject zumbi = Instantiate(zumbip);
+
+        Vector2 posicaoa = Random.insideUnitCircle * area;
+
+        zumbi.transform.position = new Vector3(posicaoa.x, zumbi.transform.position.y, posicaoa.y);
+    }
 }
--- Assets/Scripts/Zumbi.cs
+++ Assets/Scripts/Zumbi.cs
@@ -1,19 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Zumbi : MonoBehaviour
 {
-    public Vector3 play;
+    public Transform play;
+    private Animator an;
+    private Rigidbody rb;
+    public float v = 0.35f;
+    private bool morto = false;
+    private GameObject jogador;
 
     // Use this for initialization
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        an = GetComponent<Animator>();
+        jogador = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     private void Update()
     {
-        transform.LookAt(play);
+        if (!morto)
+        {
+            rb.velocity = transform.forward * v;
+            //transform.LookAt(play);
+            transform.LookAt(jogador.transform);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!morto && other.tag == "Projetil")
+        {
+            Destroy(other.gameObject);
+            Morrer();
+        }
+    }
+
+    private void OnCollisioEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.buildIndex);
+        }
+    }
+
+    private void Morrer()
+    {
+        morto = true;
+        an.SetTrigger("Morrer");
     }
 }
